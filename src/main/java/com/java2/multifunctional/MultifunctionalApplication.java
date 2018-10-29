772f47550d8f13736f35a5ac5ea0b9d89d1e97c0
Gent Hoxha <gent.hoxha@scoopandspoon.com>
2018-10-29 15:34:29 +0100

    Starting..

---
+++ src/main/java/com/java2/multifunctional/MultifunctionalApplication.java
@@ -0,0 +1,12 @@
+package com.java2.multifunctional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultifunctionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultifunctionalApplication.class, args);
+    }
+}
---
+++ src/test/java/com/java2/multifunctional/MultifunctionalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java2.multifunctional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultifunctionalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
